7f17f33bab7e5804fbe58ea8f1ae420da836b084
Almirderland <xalitova.almira@list.ru>
2021-04-25 22:58:51 +0600

    last

---
+++ 2attestation/python create_table.py
@@ -0,0 +1,9 @@
+suppliers=# \dt
+             List of relations
+ Schema |     Name      | Type  |  Owner
+--------+---------------+-------+----------
+ public | part_drawings | table | postgres
+ public | parts         | table | postgres
+ public | vendor_parts  | table | postgres
+ public | vendors       | table | postgres
+(4 rows)
